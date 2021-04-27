da401b5a4ee6c4645ae680d433354da76906157a
alex4jdm <alex4jdm@gmail.com>
2021-04-27 22:02:49 +0300

    Added basic REST api.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+
+const port = 8000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, { useUnifiedTopology: true }, (err, database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app, database.db('Cluster0'));
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
+    });               
+  })
