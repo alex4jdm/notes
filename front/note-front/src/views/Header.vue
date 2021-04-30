859e4d61a78ffa749518e7907586c213834dac14
PliskoBogdan <fanatamas64@gmail.com>
2021-04-30 18:12:27 +0300

    4: minor commit

--- front/note-front/src/views/Header.vue
+++ front/note-front/src/views/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar-brand href="#">Notes App</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
