97aac5083b912ef1de845fa730e9ffcd1c3c26c0
PliskoBogdan <fanatamas64@gmail.com>
2021-05-08 00:12:11 +0300

    4: add validations

--- front/note-front/src/views/FlowCreateNote.vue
+++ front/note-front/src/views/FlowCreateNote.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="home">
-    <div>
-      <h3>Note Title</h3>
-      <b-form-input v-model="form.title" type="text" />
-    </div>
-    <div>
-      <h4>Note Body</h4>
-      <b-form-input v-model="form.body" type="text" />
-    </div>
+    <form @submit.prevent="onSubmitForm">
+      <b-row>
+        <b-col class="d-flex flex-column align-items-center">
+          <h4>Note Title</h4>
+          <b-form-input v-model="form.title" type="text" />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col class="d-flex flex-column align-items-center mt-2">
+          <h4>Note Body</h4>
+          <b-form-input v-model.trim="$v.form.$body" type="text" />
+          <div class="error" v-if="!$v.form.body.required">Body is required</div>
+        </b-col>
+      </b-row>
+    </form>
     <div class="create-flow-button-submit">
-      <b-button :disabled="!form.title || !form.body" @click="onSubmitForm">Submit</b-button>
+      <b-button :disabled="!form.title || !form.body" @click="onSubmitForm"
+        >Submit</b-button
+      >
     </div>
   </div>
 </template>
 
 <script>
-import generalService from '../services/general-services'
+import generalService from "../services/general-services";
+import { required } from "vuelidate/lib/validators";
 
 export default {
   data() {
@@ -23,8 +33,15 @@ export default {
       form: {
         title: null,
         body: null,
-      }
-    }
+      },
+    };
+  },
+
+  validations: {
+    form: {
+      title: { required },
+      body: { required },
+    },
   },
 
   methods: {
@@ -32,21 +49,21 @@ export default {
       generalService.createNotes(this.form).then(() => {
         this.form = null;
         this.body = null;
-      })
-    }
+      });
+    },
   },
-}
+};
 </script>
 
 <style>
-.home{
+.home {
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;  
+  align-items: center;
 }
-.create-flow-button-submit{
+.create-flow-button-submit {
   margin-top: 1.5em;
   cursor: pointer;
 }
